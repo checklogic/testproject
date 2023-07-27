6694f58e2a00915d09a5f16c04532264796a7cc9
findLogic <fun-elco@list.ru>
2023-07-27 14:12:53 +0700

    7. Добавили темы

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { createRoot } from 'react-dom/client';
+import ThemeProvider from './theme/ThemeProvider';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
 	<BrowserRouter>
-		<App />
+		<ThemeProvider>
+			<App />
+		</ThemeProvider>
 	</BrowserRouter>
 );
---
+++ src/theme/ThemeProvider.tsx
@@ -0,0 +1,29 @@
+import { FC, FunctionComponent, ReactNode, useMemo, useState } from 'react';
+import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from './ThemeContext';
+
+const defaultTheme =
+	(localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme) || Theme.LIGTH;
+
+interface ThemeProviderProps {
+	children: ReactNode;
+}
+
+const ThemeProvider: FunctionComponent<ThemeProviderProps> = ({ children }) => {
+	const [theme, setTheme] = useState<Theme>(defaultTheme);
+
+	const defaultProps = useMemo(
+		() => ({
+			theme,
+			setTheme,
+		}),
+		[theme]
+	);
+
+	return (
+		<ThemeContext.Provider value={defaultProps}>
+			{children}
+		</ThemeContext.Provider>
+	);
+};
+
+export default ThemeProvider;
