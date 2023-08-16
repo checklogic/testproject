59b9e9686a656133347cfac4c8baac1200c65898
checklogic <elcomework@yandex.ru>
2023-08-16 13:30:30 +0700

    fix stories

---
+++ src/widgets/Page/ui/Page.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Page } from './Page';
+import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator';
+
+const meta = {
+    component: Page,
+    title: 'widgets/Page',
+    decorators: [StoreDecorator({})],
+} satisfies Meta<typeof Page>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+    args: {
+        children: 'test',
+    },
+};
