339984e02a27acd61ab4a271ce414cf40683cef3
checklogic <elcomework@yandex.ru>
2023-08-20 13:32:51 +0700

    jest reports

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -32,6 +32,18 @@ const config: Config = {
         __API__: '',
         __PROJECT__: 'jest',
     },
+    reporters: [
+        'default',
+        [
+            'jest-html-reporters',
+            {
+                publicPath: `<rootDir>/reports/unit`,
+                filename: 'report.html',
+                openReport: true,
+                inlineSource: true,
+            },
+        ],
+    ],
 
     // All imported modules in your tests should be mocked automatically
     // automock: false,
